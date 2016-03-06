324d1d53f50d3b7a7e4c15d78f5c93dfc2a77ed3
chepiga1995 <ura1995ura@gmail.com>
2016-03-06 02:14:32 +0200

    change config

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,2 +1,10 @@
-cd /home/ubuntu/snail &&
+PROJECT_DIRECTORY=/home/ubuntu/snail/
+ENV=$PROJECT_DIRECTORY.env
+
+if [ -f $ENV ]; then
+    echo "File .env found!"
+	export $(cat $ENV)
+fi
+
+cd $PROJECT_DIRECTORY &&
 forever start ./server.js
