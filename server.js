6d4b41c9ee97d9c824c941b8d891ad00fbb4859f
chepiga1995 <ura1995ura@gmail.com>
2016-08-15 17:13:26 +0300

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var init = require('./config/init')(),
 	mongoose = require('mongoose'),
 	chalk = require('chalk');
 
-/**
+/** hj
  * Main application entry file.
  * Please note that the order of loading is important.
  */
